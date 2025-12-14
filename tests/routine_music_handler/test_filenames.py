e9998e07ca2913893bd780e57e84abd368ad0552
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-12-14 14:45:08 -0600

    Fixing test

--- tests/routine_music_handler/test_filenames.py
+++ tests/routine_music_handler/test_filenames.py
@@ -1,5 +1,9 @@
-from routine_music_handler.filenames import _parse_season_year, build_base_filename
-from routine_music_handler.submission_schema import Submission
+from routine_music_handler.file_translations import (
+    _parse_season_year,
+    build_base_filename,
+    sanitize_part,
+)
+from routine_music_handler.sheet_state import Submission
 
 
 def test_season_year_november_rolls_forward():
@@ -22,6 +26,13 @@ def test_base_filename_includes_year_and_optional_fields():
         personal_descriptor="Sparkly Shoes",
         audio_url="https://drive.google.com/file/d/1234567890123456789012345/view",
     )
-    base, year = build_base_filename(sub)
+    base, year = build_base_filename(
+        timestamp=sub.timestamp,
+        leader=sanitize_part(sub.leader_first) + sanitize_part(sub.leader_last),
+        follower=sanitize_part(sub.follower_first) + sanitize_part(sub.follower_last),
+        division=sanitize_part(sub.division),
+        routine=sanitize_part(sub.routine_name),
+        descriptor=sanitize_part(sub.personal_descriptor),
+    )
     assert year == "2025"
     assert base.endswith("_2025_SparklyShoes")
