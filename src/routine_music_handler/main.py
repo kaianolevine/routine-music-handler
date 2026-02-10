564bcf02e5e2f63fe8705dd2b40a0e042c82fbed
Kaiano Levine <kaiano.levine@gmail.com>
2026-02-10 16:55:25 -0600

    Running twice from two sources to support new and legacy submit form

--- src/routine_music_handler/main.py
+++ src/routine_music_handler/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import sys
 
 from kaiano.google import GoogleAPI
@@ -9,11 +10,18 @@ from routine_music_handler.processor import process_submission_sheet
 
 def main() -> int:
 
-    worksheet_name = "Form Responses 1"
-    dest_root_folder_id = "1I7NxCM4RLYhXmQ1BaQjsNvKNWgMClgLH"
-    submission_sheet_id = "1V2hupjlrsCtOTU4p_hALCTRKDaB4We84tLnGUCecLLU"
-    submissions_folder_id = (
-        "1CeUNN08N5SMgZf1RQGHfqPO_ZyrAJzc0M_i2xZCeVuR-TAbnDrncnz9CGJHWY4Af1IecJMi9"
+    worksheet_name = os.getenv("ROUTINE_MUSIC_WORKSHEET_NAME", "Form Responses 1")
+    dest_root_folder_id = os.getenv(
+        "ROUTINE_MUSIC_DEST_ROOT_FOLDER_ID",
+        "1I7NxCM4RLYhXmQ1BaQjsNvKNWgMClgLH",
+    )
+    submission_sheet_id = os.getenv(
+        "ROUTINE_MUSIC_SUBMISSION_SHEET_ID",
+        "1V2hupjlrsCtOTU4p_hALCTRKDaB4We84tLnGUCecLLU",
+    )
+    submissions_folder_id = os.getenv(
+        "ROUTINE_MUSIC_SUBMISSIONS_FOLDER_ID",
+        "1CeUNN08N5SMgZf1RQGHfqPO_ZyrAJzc0M_i2xZCeVuR-TAbnDrncnz9CGJHWY4Af1IecJMi9",
     )
 
     g = GoogleAPI.from_env()
